5da85e5ab59f68b73bc2e97b62d0816e7be9e2fc
vanVanh <108987002+vanVanh@users.noreply.github.com>
2023-04-09 17:09:08 +0700

    api categories

--- contronller/api/sanpham.api.controller.js
+++ contronller/api/sanpham.api.controller.js
@@ -12,3 +12,16 @@ exports.listProduct = async (req, res, next) => {
         return res.status(500).json({ msg: error.message })
     }
 }
+
+exports.listCategory = async (req, res, next) => {
+    try {
+        let list = await model.category.find();
+        if (list) {
+            return res.status(200).json({data: list, msg: 'lấy dữ liệu thành công'});
+        }else{
+            return res.status(204).json({msg: 'không có dữ liệu'});
+        }
+    } catch (error) {
+        return res.status(500).json({ msg: error.message })
+    }
+}
--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ var apiCtrl = require('../contronller/api/sanpham.api.controller');
 
 /* GET home page. */
 router.get('/products/list', apiCtrl.listProduct);
+router.get('/categories/list', apiCtrl.listCategory);
 
 module.exports = router;
